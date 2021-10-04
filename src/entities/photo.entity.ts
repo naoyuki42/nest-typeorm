16f0458f754cb810c2e784ea48d728347095e686
宮森尚志 <naoyuki.7042@outlook.jp>
2021-10-04 16:23:06 +0900

    fix:

--- src/entities/photo.entity.ts
+++ src/entities/photo.entity.ts
@@ -1,13 +1,33 @@
-import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+import {
+  Entity,
+  BaseEntity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  ManyToOne,
+  JoinColumn,
+} from 'typeorm';
+import { User } from './user.entity';
 
 @Entity()
-export class Photo {
+export class Photo extends BaseEntity {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column()
+  @Column({ name: 'subject' })
   subject: string;
 
-  @Column()
-  userId: string;
+  @Column({ name: 'genru' })
+  genru: string;
+
+  @ManyToOne(type => User, user => user.photos)
+  @JoinColumn({ name: 'user_id' })
+  user: User;
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ name: 'updated_at' })
+  updatedAt: Date;
 }
--- src/entities/user.entity.ts
+++ src/entities/user.entity.ts
@@ -1,16 +1,31 @@
-import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+import {
+  Entity,
+  BaseEntity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  OneToMany,
+} from 'typeorm';
+import { Photo } from './photo.entity';
 
 @Entity()
-export class Users {
+export class User extends BaseEntity {
   @PrimaryGeneratedColumn()
   id: number;
 
-  @Column()
-  firstName: string;
+  @Column({ name: 'name' })
+  name: string;
 
-  @Column()
-  lastName: string;
-
-  @Column()
+  @Column({ name: 'age' })
   age: number;
+
+  @OneToMany(type => Photo, photo => photo.user)
+  photos: Photo;
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: Date;
+
+  @UpdateDateColumn({ name: 'updated_at' })
+  updatedAt: Date;
 }
