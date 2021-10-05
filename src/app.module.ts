28712e75b4ae6a39d153eac7894f44fa6c43ee01
宮森尚志 <naoyuki.7042@outlook.jp>
2021-10-05 10:12:50 +0900

    add: 各種クラスの追加

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './app/user/user.module';
+import { PhotoModule } from './app/photo/photo.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot()],
+  imports: [TypeOrmModule.forRoot(), UserModule, PhotoModule],
   controllers: [AppController],
   providers: [AppService],
 })
