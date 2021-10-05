76eb580b17f62d51c7f92217689e9c6c0215d103
宮森尚志 <naoyuki.7042@outlook.jp>
2021-10-05 10:43:06 +0900

    add: コントローラー作成

--- src/app/user/user.controller.ts
+++ src/app/user/user.controller.ts
@@ -1,4 +1,44 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Delete,
+  Get,
+  Post,
+  Put,
+  Param,
+  Body,
+} from '@nestjs/common';
+import { UserService } from './user.service';
+import { User } from '../../entities/user.entity';
+import { UserDto } from './user.dto';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+
+  constructor(private readonly userService: UserService) {}
+
+  @Get()
+  findAllUser(): Promise<User[]> {
+    return this.userService.findAll();
+  }
+
+  @Get('/:id')
+  findOneUser(@Param('id') id: number): Promise<User> {
+    return this.userService.find(id);
+  }
+
+  @Post()
+  createUser(@Body() userData: UserDto): Promise<User> {
+    return this.userService.create(userData);
+  }
+
+  @Put('/:id')
+  updateUser(@Param('id') id: number): Promise<User> {
+    return this.userService.update(id);
+  }
+
+  @Delete('/:id')
+  deleteUser(@Param('id') id: number): Promise<User> {
+    return this.userService.delete(id);
+  }
+
+}
